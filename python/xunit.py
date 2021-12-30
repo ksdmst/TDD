d3c30cbf76c23e47bfc1e41dad16d2d773a8f700
kusuda_m <kusudamasato@yahoo.co.jp>
2021-12-30 18:30:52 +0900

    chapter 18-python
    chapter 18-python

---
+++ python/xunit.py
@@ -0,0 +1,33 @@
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+
+    def run(self):
+        method = getattr(self, self.name)
+        method()
+
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.wasRun = None
+        super().__init__(name)
+
+    def testMethod(self):
+        self.wasRun = 1
+
+
+class TestCaseTest(TestCase):
+    def testRunnig(self):
+        test = WasRun("testMethod")
+        assert not test.wasRun
+        test.run()
+        assert test.wasRun
+
+    def testSetUp(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert test.wasRun
+
+
+TestCaseTest("testRunnig").run()
+TestCaseTest("testSetUp").run()
